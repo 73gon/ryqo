5584cef41a256ff1f13f06b40fa1308feb437ffa
73gon <malikmardan03@gmail.com>
2025-11-20 02:59:50 +0100

    fix import casing for Navbar and Footer components

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,9 +2,9 @@ import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 
-import { Navbar } from '@/components/navbar'
+import { Navbar } from '@/components/Navbar'
 import { Toaster } from '@/components/ui/sonner'
-import { Footer } from '@/components/footer'
+import { Footer } from '@/components/Footer'
 
 export const Route = createRootRoute({
   component: () => (
